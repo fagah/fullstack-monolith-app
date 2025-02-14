c9a42130d35ba75b84e837a85fb03f7b95693ab6
Franck Janel AGAH <fagah@users.noreply.github.com>
2025-02-13 23:02:43 -0500

    Update AppModule with Keycloak integration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,9 @@ import { AppointmentModule } from './appointment/appointment.module';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
 
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { initializeKeycloak } from './app.initializer';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -23,6 +27,7 @@ import { PatientModule } from './patient/patient.module';
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    KeycloakAngularModule,
     AppRoutingModule,
     CoreModule,
     SharedModule,
@@ -32,7 +37,19 @@ import { PatientModule } from './patient/patient.module';
     DoctorModule,
     PatientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
